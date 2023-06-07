527877c466584e3da3501f0f826a7959c17d447f
qingwave <>
2023-06-07 06:23:27 +0000

    Update from https://github.com/qingwave/writings/commit/9e2206c86df07e5d08cc4d338fcade3a5390df0c

---
+++ _astro/hoisted.9e8197df.js
@@ -0,0 +1,1 @@
+import"./hoisted.fa5a2e6c.js";window.addEventListener("DOMContentLoaded",e=>{new PagefindUI({element:"#search",resetStyles:!1})});window.addEventListener("keydown",e=>{(e.key==="/"||e.key===".")&&(console.log("you pressed the slash"),e.preventDefault(),document.querySelector("div#search input").focus())});
